cfdf8ccf32ae94f7e1593a04d66c47abcc57c856
Nytheneals <nythes@gmail.com>
2018-01-23 15:09:16 -0500

    Updated Package.json,installed create-react-class (Outdated) and Created a simple Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,31 @@
-alert("Wooooow");
+var React = require("react");
+var ReactDOM = require("react-dom");
+var createReactClass = require("create-react-class");
+
+//Create a component
+var TodoComponent = createReactClass({
+  getInitialState: function() {
+    return {
+      todos: ["wash", "eat", "sleep"]
+    };
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>
+          Name:<span>{this.state.todos[0]}</span>
+        </h1>
+        <h1>
+          Job:<span>{this.state.todos[1]}</span>
+        </h1>
+        <h1>
+          Location:<span>{this.state.todos[2]}</span>
+        </h1>
+      </div>
+    );
+  }
+});
+
+// DOM ELEMENT
+var node = document.getElementById("todo_wrapper");
+ReactDOM.render(<TodoComponent />, node);
